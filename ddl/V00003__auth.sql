eda79cc799fe5f689137767d6090c0f59193ecbe
mclaughlint <tim.t.mclaughlin@gmail.com>
2021-04-14 14:56:54 -0600

    removing auth  db script

--- ddl/V00003__auth.sql
+++
@@ -1,8 +0,0 @@
--- create users table for auth
-
-CREATE TABLE IF NOT EXISTS api.auth_users
-(
-    id       SERIAL PRIMARY KEY,
-    username TEXT         NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
